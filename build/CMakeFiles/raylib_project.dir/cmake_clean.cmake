188f95dade79c51760b7a65e0e5bda2d6f99a82c
Edge Lord <kengehssan@gmail.com>
2025-10-08 15:03:31 +0200

    all of this for just a build

---
+++ build/CMakeFiles/raylib_project.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/raylib_project.dir/src/main.c.o"
+  "CMakeFiles/raylib_project.dir/src/main.c.o.d"
+  "raylib_project"
+  "raylib_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/raylib_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/raylib_project.dir/src/main.c.o.d
@@ -0,0 +1,5 @@
+CMakeFiles/raylib_project.dir/src/main.c.o: \
+ /home/edge/Desktop/raylib/src/main.c /usr/include/stdc-predef.h \
+ /home/edge/Desktop/raylib/include/raylib.h \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/stdarg.h \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/stdbool.h
